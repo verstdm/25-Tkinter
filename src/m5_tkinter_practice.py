bc5e4cbeffc307a8dafc45de413dce930b1f062f
verstdm <verstdm@rose-hulman.edu>
2018-10-31 16:41:31 -0400

    DONE

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -2,47 +2,75 @@
 This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Dylan Verst.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
 
 
+def print_hello():
+    print('Hello')
+
+
+def print_me(contents):
+    if contents == 'ok':
+        print('Hello')
+    else:
+        print('Goodbye')
+
+
+def fun(contents, n):
+    for k in range(int(n)):
+        print(contents)
+
+
 def main():
     """ Constructs a GUI with stuff on it. """
     # ------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # ------------------------------------------------------------------
+    root = tkinter.Tk()
 
     # ------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # ------------------------------------------------------------------
-
+    frame1 = ttk.Frame(root, padding=20)
+    frame1.grid()
     # ------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # ------------------------------------------------------------------
-
-    # ------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    button = ttk.Button(frame1, text='Press Me!')
+    button['command'] = (lambda:
+                         print_hello())
+    button.grid()
+    # -------------------------------------
+    # -----------------------------
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # ------------------------------------------------------------------
 
     # ------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # ------------------------------------------------------------------
+    entry = ttk.Entry(frame1)
+    entry.grid()
+    button2 = ttk.Button(frame1, text='No! Press Me!')
+    button2['command'] = (lambda:
+                          print_me(entry.get()))
+    button2.grid()
 
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -64,10 +92,18 @@ def main():
     #      s = entry_box.get()
     #      n = int(s)
     ####################################################################
+    entry2 = ttk.Entry(frame1)
+    entry2.grid()
+    button3 = ttk.Button(frame1, text='NOOOOOOO! Press Meeeee!')
+    button3['command'] = (lambda:
+                          fun(entry.get(), entry2.get()))
+    button3.grid()
 
     # ------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DONE: 8. As time permits, do other interesting GUI things!
     # ------------------------------------------------------------------
+
+    root.mainloop()
 
 
 # ----------------------------------------------------------------------
